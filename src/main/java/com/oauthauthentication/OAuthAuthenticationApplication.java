3780e2c140eb1336cb7ba89fcdca683560b84c28
lbrav <l.braverman.l@live.co.uk>
2021-08-31 14:02:49 +0100

    Added dependencies

---
+++ src/main/java/com/oauthauthentication/OAuthAuthenticationApplication.java
@@ -0,0 +1,15 @@
+package com.oauthauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class OAuthAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oauthauthentication/OAuthAuthenticationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.oauthauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
